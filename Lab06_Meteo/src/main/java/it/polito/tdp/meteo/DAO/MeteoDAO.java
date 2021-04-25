e3983538571a88a238a3c0d258c7b60c859ece1f
Manuel Taglieri <s257476@studenti.polito.it>
2021-04-25 13:07:48 +0200

    Punto 1

--- Lab06_Meteo/src/main/java/it/polito/tdp/meteo/DAO/MeteoDAO.java
+++ Lab06_Meteo/src/main/java/it/polito/tdp/meteo/DAO/MeteoDAO.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,7 +26,7 @@ public class MeteoDAO {
 
 			while (rs.next()) {
 
-				Rilevamento r = new Rilevamento(rs.getString("Localita"), rs.getDate("Data"), rs.getInt("Umidita"));
+				Rilevamento r = new Rilevamento(rs.getString("Localita"), rs.getDate("Data").toLocalDate(), rs.getInt("Umidita"));
 				rilevamenti.add(r);
 			}
 
@@ -40,9 +41,66 @@ public class MeteoDAO {
 	}
 
 	public List<Rilevamento> getAllRilevamentiLocalitaMese(int mese, String localita) {
-
-		return null;
+		
+		String sql = "SELECT Umidita, Data "
+				+ "FROM situazione "
+				+ "WHERE Localita=? AND MONTH(Data)=?";
+		
+		try {
+			
+			Connection conn = ConnectDB.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql); 
+			st.setString(1, localita);
+			st.setInt(2, mese);
+			ResultSet rs = st.executeQuery();
+			
+			List<Rilevamento> risultato = new ArrayList<Rilevamento>();
+			
+			while (rs.next()) {
+				LocalDate data = rs.getDate("Data").toLocalDate();
+				risultato.add(new Rilevamento(localita, data, rs.getInt("Umidita")));
+			}
+			
+			rs.close();
+			st.close();
+			conn.close();
+			
+			return risultato;
+			
+		} catch(SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
 	}
-
+	
+	public List<String> getCitta() {
+		String sql = "SELECT DISTINCT Localita "
+				+ "FROM situazione";
+		
+		try {
+			
+			Connection conn = ConnectDB.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql); 
+			ResultSet rs = st.executeQuery();
+			
+			List<String> risultato = new ArrayList<String>();
+			
+			while (rs.next()) {
+				risultato.add(rs.getString("Localita"));
+			}
+			
+			rs.close();
+			st.close();
+			conn.close();
+			
+			return risultato;
+			
+		} catch(SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
+	
+	
 
 }
--- Lab06_Meteo/src/main/java/it/polito/tdp/meteo/model/Model.java
+++ Lab06_Meteo/src/main/java/it/polito/tdp/meteo/model/Model.java
@@ -1,6 +1,12 @@
 package it.polito.tdp.meteo.model;
 
 import java.util.List;
+import java.util.Map;
+
+import it.polito.tdp.meteo.DAO.MeteoDAO;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Model {
 	
@@ -8,14 +14,50 @@ public class Model {
 	private final static int NUMERO_GIORNI_CITTA_CONSECUTIVI_MIN = 3;
 	private final static int NUMERO_GIORNI_CITTA_MAX = 6;
 	private final static int NUMERO_GIORNI_TOTALI = 15;
+	
+	private MeteoDAO dati;
+	private List<Citta> citta;
+	private List<Rilevamento> rilevamenti;
 
 	public Model() {
+		
+		this.dati = new MeteoDAO();
+		this.citta = new ArrayList<Citta>();
+		
+		for (String s : dati.getCitta()) {
+			citta.add(new Citta(s));
+		}
+		
+		this.rilevamenti = dati.getAllRilevamenti();
+		
+		for (Citta c : this.citta) {
+			List<Rilevamento> rivPerCitta = new ArrayList<Rilevamento>();
+			for (Rilevamento r : this.rilevamenti) {
+				if (c.getNome().equals(r.getLocalita()))
+					rivPerCitta.add(r);
+			}
+			c.setRilevamenti(rivPerCitta);
+		}
 
 	}
 
 	// of course you can change the String output with what you think works best
 	public String getUmiditaMedia(int mese) {
-		return "TODO!";
+		
+		String risStringa="";
+		
+		for (Citta c : citta) {
+			List<Rilevamento> ris = dati.getAllRilevamentiLocalitaMese(mese, c.getNome());
+			float somma = 0;
+			for (Rilevamento r : ris) {
+				somma += r.getUmidita();
+			}
+			float media = somma/ris.size();
+			media = Math.round(media);
+			risStringa += c.getNome() + ": " + media + "\n";
+		}
+	
+		return risStringa;
 	}
 	
 	// of course you can change the String output with what you think works best
